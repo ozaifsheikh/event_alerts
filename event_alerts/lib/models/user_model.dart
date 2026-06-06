d0c490b0b3c6f90cd4966f6a59f2bcd0ab065ff9
ozaifsheikh <ozaifsheikh008@gmail.com>
2026-06-06 17:28:26 +0500

    dmin setting complete

---
+++ event_alerts/lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final String id;
+  final String name;
+  final bool isAdmin;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.isAdmin,
+  });
+}
